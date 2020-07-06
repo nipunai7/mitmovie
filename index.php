3f75e08e00de09a42cc5141a9a2881ca5701cb49
Nipuna Muansinghe <mnipunai7@gmail.com>
2020-07-06 09:22:57 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Kevin with Gayathri Official">
+  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="author" content="MNK">
     <title>Kevin with Gayathri</title>
 
     <!-- Bootstrap -->
