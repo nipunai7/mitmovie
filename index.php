e6bc52381bc7ccac0cf912bf3b805bed409974fd
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-06 18:27:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <meta property="twitter:description" content="All the fun videos of Kevin's Aunt love fantasies  ...">
 <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
   <meta name="author" content="MNK">
-    <title>Kevin with Gayathri</title>
+    <title>MNK with Gayathri</title>
     
 
     <!-- Bootstrap -->
@@ -37,7 +37,7 @@
   </head>
   <h1>mewa MNK ge WADA BN</h1>
   <body><div style="text-align:center;">
-    <h1>Kevin & Gayathri Movie Collection!</h1>
+    <h1>MNK & Gayathri Movie Collection!</h1>
 <p>Coming Soon!!!</p>
 <p>All the fun videos of Kevin's Aunt love fantasies  ...</p>
     <p>for more details...</p>
