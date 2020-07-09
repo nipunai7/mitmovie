d66b691674624fd4ae1bb59f68a8bf62e063f7ec
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-09 18:37:32 +0530

    jw

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
                     <h3 class="name">Article Title</h3>
                     <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a></div>
                 <div
-                    class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="/assets/img/desk.jpg"></a>
+                    class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="/assets/img/jw.jpg"></a>
                     <h3 class="name">Article Title</h3>
                     <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a></div>
             <div
