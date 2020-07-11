3ff3c2edbdf20520ada288b16a517ca6055b2ff0
Kisal Perera <59250640+kisalperera@users.noreply.github.com>
2020-07-11 22:28:21 +0530

    Add files via upload

---
+++ reg form/regform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="rg.css">
+</head>
+<body>
+	<div class = "group1">
+		<label >Name   :</label><br>
+		<input type="text" name="name" placeholder="name..."><br><br>
+		<label>Email      :</label><br>
+		<input type="Email" name="email" placeholder="email..."><br><br>
+		<label>Password :</label><br>
+		<input type="Password" name="passw" placeholder="password..."><br><br>
+ 		<label>Re - Enter Password :</label><br>
+ 		<input type="Password" name="repassw" placeholder="re-enter password..."><br><br>
+
+ 		</div>
+ 	<div class = "group2">
+ 		<input type="Checkbox" name="remindme"><label>Remember Me</label><br><br>
+ 		<input type="button" name="cancel" value="Cancel">
+ 		<input type="button" name="sign" value="Sign Up">
+ 		
+
+
+	</div>
+
+</body>
+</html>
