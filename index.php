2ee204371d2e2263f1abdd6b2af9bce6470427d8
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-12 15:26:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
                     <li class="nav-item" role="presentation"><a class="nav-link" href="login.html">Login</a></li>
                     <!-- <li class="nav-item" role="presentation"><a class="nav-link" href="#">Music</a></li> -->
                     <li class="nav-item" role="presentation"><a class="nav-link" href="index.html">Sign Up</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="signup.html"><i class="icon ion-ios-search-strong"></i></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="signp.html"><i class="icon ion-ios-search-strong"></i></a></li>
                 </ul>
             </div>
         </div>
