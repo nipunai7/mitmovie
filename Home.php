cd785a2ccd8a4b8a67acf316c5cff4e719768927
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-12 18:31:07 +0530

    update

--- Home.php
+++ Home.php
@@ -29,8 +29,8 @@
     <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="/assets/css/Swipe-Slider-9.css">
 
-    <script src="js/main.js"></script>
     
+
 </head>
 
 <body  >
@@ -151,6 +151,27 @@
                         <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
                         <script src="/assets/js/Simple-Slider.js"></script>
                         <script src="/assets/js/Swipe-Slider-9.js"></script>
+
+
+
+
+                          <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+
+
 </body>
 
 </html>
