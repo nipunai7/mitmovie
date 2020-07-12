6f8ac82d5f3f8861df128f0deb37d8f422180c12
Nipuna Muansinghe <mnipunai7@gmail.com>
2020-07-12 23:17:16 +0530

    sign up CSS error 1

--- header.php
+++ header.php
@@ -28,38 +28,17 @@
 		<link rel="stylesheet" href="/assets/css/sticky-dark-top-nav-with-dropdown.css">
 		<link rel="stylesheet" href="/assets/css/styles.css">
 		<link rel="stylesheet" href="/assets/css/Swipe-Slider-9.css">
-
-
-
-
-
-
-
-
-
-
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+    
 	<link rel="icon" type="image/png" href="/assets/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/animate/animate.css">
-<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="/assets/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/assets/bootstrap/css/util.css">
 	<link rel="stylesheet" type="text/css" href="/assets/bootstrap/css/main.css">
-<!--===============================================================================================-->
+
 </head>
