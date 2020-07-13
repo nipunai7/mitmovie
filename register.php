5ce98c4cf838da09c0202912f2cf4f2089b0392d
Nipuna Muansinghe <mnipunai7@gmail.com>
2020-07-13 12:34:19 +0530

    databse connected and working

--- register.php
+++ register.php
@@ -14,10 +14,10 @@ if(isset($_POST['submit'])){
         <div class="form-container">
             <div class="image-holder"></div>
             <form method="POST" action="register.php" name="reg">
-            <?php include('errors.php'); ?>
+            <?php #include('errors.php'); ?>
                 <h2 class="text-center"><strong>Create</strong> an account.</h2>
                 <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Username" id="name"></div>
-                <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email" id="email"></div>
+                <div class="form-group"><input class="form-control" type="text" name="email" placeholder="Email" id="email"></div>
                 <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password" id="password"></div>
                 <div class="form-group"><input class="form-control" type="password" name="password-repeat" placeholder="Password (repeat)" id="password-repeat"></div>
                 <div class="form-group">
--- server.php
+++ server.php
@@ -1,23 +1,23 @@
 <?php
 
-$name = "";
-$email = "";
-$errors = array();
+// $name = "";
+// $email = "";
+// $errors = array();
 
-$db = mysqli_connect('139.162.10.226', 'mitmovie', 'mitmovie', 'mitmovie');
+// $db = mysqli_connect('139.162.10.226', 'mitmovie', 'mitmovie', 'mitmovie');
 
-if ($db->connect_error) {
-	die("Connection failed: " . $db->connect_error);
-  }
-  echo "Connected successfully";
+// if ($db->connect_error) {
+// 	die("Connection failed: " . $db->connect_error);
+//   }
+//   echo "Connected successfully";
 
-  $mysqli = new mysqli('139.162.10.226', 'mitmovie', 'mitmovie', 'mitmovie');
+//   $mysqli = new mysqli('139.162.10.226', 'mitmovie', 'mitmovie', 'mitmovie');
 
 // if(isset($_POST['reg'])){
-    $name = $mysqli->real_escape_string($_POST['name']);
-    $email = $mysqli->real_escape_string($_POST['email']);
-    $password_1 = $mysqli->real_escape_string($_POST['password']);
-    $password_2 = $mysqli->real_escape_string($_POST['password-repeat']);
+//     $name = $mysqli->real_escape_string($_POST['name']);
+//     $email = $mysqli->real_escape_string($_POST['email']);
+//     $password_1 = $mysqli->real_escape_string($_POST['password']);
+//     $password_2 = $mysqli->real_escape_string($_POST['password-repeat']);
 
 // if(empty($name)){
 // 	array_push($errors,"username is required");
@@ -38,9 +38,41 @@ if ($db->connect_error) {
 	
 // }
 
-$sql= "INSERT INTO user(username, email, pwd) VALUES ('$name', '$email', '$password_1')";
+// $sql= "INSERT INTO user(name, email, pwd) VALUES ('mnk', 'mnk', 'mnk')";
 
-mysqli_query($db,$sql);
+// $mysqli->query($sql);
+// mysqli_query($sql);
 // }
+
+
+$servername = "139.162.10.226";
+$username = "mitmovie";
+$password = "mitmovie";
+$dbname = "mitmovie";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $conn->real_escape_string($_POST['name']);
+$email = $conn->real_escape_string($_POST['email']);
+$password_1 = $conn->real_escape_string($_POST['password']);
+$password_2 = $conn->real_escape_string($_POST['password-repeat']);
+
+$sql = "INSERT INTO user (name, email, pwd)
+VALUES ('$name', '$email', '$password_1')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
 
 ?>
