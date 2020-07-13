a44a2a9501cc6b088860a7b47a1ef0aed46f4b9e
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-13 19:54:23 +0530

    updated

--- header.php
+++ header.php
@@ -32,16 +32,16 @@
 		<link rel="stylesheet" href="assets/bootstrap/css/styles.css">
 		<link rel="stylesheet" href="assets/bootstrap/css/Login-Form-Clean.css">
     
-	<link rel="icon" type="image/png" href="assets/images/icons/favicon.ico"/>
-	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/vendor/daterangepicker/daterangepicker.css">
-	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/util.css">
-	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/main.css">
+		<link rel="icon" type="image/png" href="assets/images/icons/favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/animsition/css/animsition.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/daterangepicker/daterangepicker.css">
+		<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/util.css">
+		<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/main.css">
 
 </head>
