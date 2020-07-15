63364a43381948ca7fb41db48b8928d4e3c9ffb1
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-15 12:11:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
 <body  >
     <?php include('navbar.php'); ?>
-    <slider>
+    <!-- <slider>
             <slide><p>slide 1</p></slide>
             <slide><p>slide 2</p></slide>
             <slide><p>slide 3</p></slide>
-        </slider>
+        </slider> -->
 
-    <!-- <div class="simple-slider">
+    <div class="simple-slider">
         <div class="swiper-container">
             <div class="swiper-wrapper" >
 
@@ -23,29 +23,9 @@
             <div class="swiper-button-prev"></div>
             <div class="swiper-button-next"></div>
         </div>
-    </div> -->
+    </div>
 
-<!-- <script>
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("swiper-slide");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-</script> -->
 
 
 
