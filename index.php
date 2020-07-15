9356bb9535cea8174d376294c8b7d703a9607120
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-15 12:05:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <meta name="viewpoint" content="width=device-width , initial-scale= 1.0">
 <meta http-equiv="X-UA-Compatible" content="ie-edge">
-
+<link rel="stylesheet" href="assets/bootstrap/css/style.css">
 <body  >
     <?php include('navbar.php'); ?>
 
