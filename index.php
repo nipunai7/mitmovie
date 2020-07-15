1688e2a87a06f2de59f6f1d2b2ae21982cc3174a
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-15 12:25:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php include ('header.php'); ?>
+<meta charset="UTF-8">
+
+<meta name="viewpoint" content="width=device-width , initial-scale= 1.0">
+<meta http-equiv="X-UA-Compatible" content="ie-edge">
+<title>pure css slider</title>
+<link rel="stylesheet" href="style.css">
+
 <body>
     <?php include('navbar.php'); ?>
     <slider>
