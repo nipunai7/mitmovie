2a7ed8ced08588c4c5bb72da3d470e6b0925a0b5
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-15 10:52:20 +0530

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,53 @@
       });
 
 })(jQuery);
+
+var carousel = function() {
+  $('.carousel-testimony').owlCarousel({
+    autoplay: true,
+    autoHeight: true,
+    center: true,
+    loop: true,
+    items:1,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    dots: true,
+    navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+    responsive:{
+      0:{
+        items: 1
+      },
+      600:{
+        items: 1
+      },
+      1000:{
+        items: 1
+      }
+    }
+  });
+  $('.carousel-causes').owlCarousel({
+    autoplay: true,
+    autoHeight: true,
+    center: true,
+    loop: true,
+    items:1,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    dots: true,
+    navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+    responsive:{
+      0:{
+        items: 1
+      },
+      600:{
+        items: 3
+      },
+      1000:{
+        items: 5
+      }
+    }
+  });
+};
+carousel();
