cc8b19150d5178aa8f0f986958522c8574db18ec
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2020-07-20 11:38:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -111,16 +111,6 @@
                     class="col-sm-6 col-md-3 item"><a href="#"><img class="img-fluid" src="assets/img/FF.jpg"></a>
                     <h3 class="name">Article Title</h3>
                     <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a></div>
-
-
-                <div
-                    class="col-sm-6 col-md-3 item"><a href="#"><img class="img-fluid" src="assets/img/FF.jpg"></a>
-                    <h3 class="name">Article Title</h3>
-                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a></div>
-
-
-
-
                 <div
                     class="col-sm-6 col-md-3 item"><a href="#"><img class="img-fluid" src="assets/img/BAT.jpg"></a>
                     <h3 class="name">Article Title</h3>
