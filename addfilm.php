319c005bf07e72b4930fa0572fbd8dc6c8eec193
Nipuna Muansinghe <mnipunai7@gmail.com>
2020-07-22 04:30:39 +0530

    add movie db connect

--- addfilm.php
+++ addfilm.php
@@ -1,49 +1,7 @@
 <?php
 include('header.php');
-require('connection.php');
-$out = "";
-$flag = TRUE;
+include('connection.php');
 
-if (isset($_FILES['img'])){
-    $out .= "Success <br>";
-
-    var_dump($_FILES['img']);
-
-    if($_FILES['img']['error'] == UPLOAD_ERR_OK) {
-        $out .= "No Error <br>";
-    } else {
-        $out .= "Error: ".$_FILES['img']['error'];
-        $flag = FALSE;
-    }
-
-    if ((exif_imagetype($_FILES['img']['tmp_name']))){
-        $out .= "This is a Valid image<br>";
-    }else{
-        $flag = FALSE;
-        $out .= "Not a Valid image<br>";
-    }
-
-    if($flag){
-        $ttl = $conn->real_escape_string($_POST['title']);
-        $genre = $conn->real_escape_string($_POST['genre']);
-        $year = $conn->real_escape_string($_POST['year']);
-        $desc = $conn->real_escape_string($_POST['desc']);
-        $name = $conn->real_escape_string($_FILES['img']["name"]);
-
-        $nameclr = str_replace(' ', '_', $name);
-        $newfilename = time().$nameclr;
-
-        // if(!move_uploaded_file($_FILES['img']["tmp_name"], 'uploads/'.$newfilename)){
-        //     $out .= "Failed to upload<br>";
-        // }else{
-        //     $out .= "File Uploaded<br>";
-        // }
-
-        $sql = "INSERT INTO movies (title,year,genre,description) VALUES ('$ttl','$year','$genre','$desc')";
-
-        $result = mysqli_query($conn,$sql) or die ("Failed: $sql");
-    }
-}
 ?>
 
 <body class="text-center" style="background-color: rgb(241,247,252);">
