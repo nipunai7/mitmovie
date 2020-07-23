625b3ac3608fb294f2fb323f6f063e6d920cb8dd
kisalperera <kisalperera999@gmail.com>
2020-07-23 22:00:16 +0530

    review

---
+++ review.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/review.css">
+  <title>Star rating using pure CSS</title>
+</head>
+
+<body>
+    
+    <div>
+    <label>Add a Review</label><br><br>
+    <textarea  rows="4"  name="comment" placeholder="leave your comment..."></textarea><br>
+    <input type="button" name="send" value="Send" style="margin-left: 897px;margin-bottom: 6px;margin-top: 0px;">      
+    
+
+
+    <div class="rate" style="padding-bottom: 0px;margin-bottom: 0px;margin-top: -97px;margin-right: 150px;">
+    <input type="radio" id="star5" name="rate" value="5" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="4" />
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="3" />
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="2" />
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="1" />
+    <label for="star1" title="text">1 star</label>
+    <br>
+    
+      
+    
+
+    </div>
+
+    </div>
+ 
+</body>
+
+</html>
